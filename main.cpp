8769c6b3c168f9d47d2636ea8badf6637546f076
Hettrick1 <evann.jacquot@gmail.com>
2024-04-02 22:20:07 +0200

    [add] sprites

--- main.cpp
+++ main.cpp
@@ -1,22 +1,84 @@
 #include "raylib.h"
 #include <iostream>
 
-using namespace std;
+const int WIDTH = 1080;
+const int HEIGHT = 720;
+
+Texture2D factoryBG, table, block;
+
+void Load();
+void Start();
+void Update();
+void Draw();
+void Unload();
+void center_window(float window_width, float window_height);
 
 int main() {
-
-    
-    cout << "Hello World" << endl;
-
-    InitWindow(300, 300, "My first Raylib window!");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(DARKGREEN);
-        EndDrawing();
+    Load();
+    Start();
+    while (!WindowShouldClose())
+    {
+        Update();
+        Draw();
     }
-
-    CloseWindow();
+    Unload();
     return 0;
+}
+void Load()
+{
+    InitWindow(WIDTH, HEIGHT, "Make More!");
+    SetTargetFPS(60);
+    factoryBG = LoadTexture("Sprites/MakeMoreBG.png");
+    table = LoadTexture("Sprites/TableSprite.png");
+    block = LoadTexture("Sprites/BlockSprite.png");
+}
+
+void Start()
+{
+    center_window(WIDTH, HEIGHT);
+}
+
+void Update()
+{
+
+}
+
+void Draw()
+{
+    BeginDrawing();
+    DrawTextureEx(factoryBG, Vector2{0,0}, 0, 5, WHITE);
+    DrawTextureEx(table, Vector2{ 350,200 }, 0, 3, WHITE);
+    DrawTextureEx(block, Vector2{ 350, 104 }, 0, 3, LIME);
+
+    DrawTextureEx(table, Vector2{ 600,200 }, 0, 3, WHITE);
+    DrawTextureEx(block, Vector2{ 600, 104 }, 0, 3, BLUE);
+
+    DrawTextureEx(table, Vector2{ 850,200 }, 0, 3, WHITE);
+    DrawTextureEx(block, Vector2{ 850, 104 }, 0, 3, BLUE);
+
+    DrawTextureEx(table, Vector2{ 350, 350 }, 0, 3, WHITE);
+    DrawTextureEx(block, Vector2{ 350, 254 }, 0, 3, BLUE);
+
+    DrawTextureEx(table, Vector2{ 350, 500 }, 0, 3, WHITE);
+    DrawTextureEx(block, Vector2{ 350, 404 }, 0, 3, BLUE);
+
+    ClearBackground(Color({ 255,255,255,255 }));
+    EndDrawing();
+}
+
+void Unload()
+{
+    UnloadTexture(factoryBG);
+    UnloadTexture(table);
+    UnloadTexture(block);
+    CloseWindow();
+}
+
+void center_window(float window_width, float window_height)
+{
+    int monitor = GetCurrentMonitor();
+    int monitor_width = GetMonitorWidth(monitor);
+    int monitor_height = GetMonitorHeight(monitor);
+    SetWindowPosition(monitor_width / 2 - static_cast<int>(window_width / 2),
+        monitor_height / 2 - static_cast<int>(window_height / 2));
 }
