d8adadd9828208cff3f636e4f623f93877de0800
Hettrick1 <evann.jacquot@gmail.com>
2024-04-05 10:48:32 +0200

    [wip]

--- Product.cpp
+++ Product.cpp
@@ -1,1 +1,33 @@
 #include "Product.h"
+
+Product::Product(int price, float fabricationTime, Color cubeColor, Texture2D productTexture)
+{
+    mPrice = price;
+    mFabricationTime = fabricationTime;
+    mCubeColor = cubeColor;
+    mProductTexture = productTexture;
+}
+
+Product::~Product()
+{
+}
+
+int Product::GetPrice()
+{
+    return mPrice;
+}
+
+float Product::GetFabricationTime()
+{
+    return mFabricationTime;
+}
+
+Color Product::GetCubeColor()
+{
+    return mCubeColor;
+}
+
+Texture2D Product::GetProductTexture()
+{
+    return mProductTexture;
+}
--- Product.h
+++ Product.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include "raylib.h"
+
 class Product
 {
+private:
+	int mPrice;
+	float mFabricationTime;
+	Color mCubeColor;
+	Texture2D mProductTexture;
+public:
+	Product(int price, float fabricationTime, Color cubeColor, Texture2D productTexture);
+	~Product();
+	int GetPrice();
+	float GetFabricationTime();
+	Color GetCubeColor();
+	Texture2D GetProductTexture();
 };
 
